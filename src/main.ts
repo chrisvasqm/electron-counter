b06b9df606555316e870bd3c7ceb13b255ecb562
chrisvasqm <christianvasquezmelo@gmail.com>
2024-05-11 06:06:41 -0400

    Prevent resizing

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ const createWindow = () => {
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
+    resizable: false
   });
 
   // and load the index.html of the app.
