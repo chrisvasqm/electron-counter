02f2aa54bb13520972e2fa489b88c749b30f5052
chrisvasqm <christianvasquezmelo@gmail.com>
2024-05-11 06:00:42 -0400

    Add layout

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,8 +1,21 @@
+import { Button, Typography } from '@mui/material';
+import { Stack } from '@mui/system';
+import { useState } from 'react';
 
 const Counter = () => {
-  return (
-    <div>Counter</div>
-  )
+    const [counter, setCounter] = useState(0);
+
+    return (
+        <Stack
+            direction='row'
+            alignItems='center'
+            justifyContent='center'
+            gap={2}>
+            <Button variant='contained'>-</Button>
+            <Typography variant='body1'>{counter}</Typography>
+            <Button variant='contained'>+</Button>
+        </Stack>
+    )
 }
 
 export default Counter
