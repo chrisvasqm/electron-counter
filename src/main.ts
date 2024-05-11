b78d94877b32463c4ff952f26c6e87aa4eca1cff
chrisvasqm <christianvasquezmelo@gmail.com>
2024-05-11 06:55:40 -0400

    Hide DevTools when env is not development

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ const createWindow = () => {
     height: 200,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
+      devTools: !app.isPackaged
     },
     resizable: false
   });
