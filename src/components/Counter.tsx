0e78e064403be453ccc9cb196f2284e7dee5eae3
chrisvasqm <christianvasquezmelo@gmail.com>
2024-05-11 06:02:42 -0400

    Add onClick handlers

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -5,15 +5,23 @@ import { useState } from 'react';
 const Counter = () => {
     const [counter, setCounter] = useState(0);
 
+    const handleMinus = () => {
+        setCounter(counter - 1);
+    }
+
+    const handlePlus = () => {
+        setCounter(counter + 1);
+    }
+
     return (
         <Stack
             direction='row'
             alignItems='center'
             justifyContent='center'
             gap={2}>
-            <Button variant='contained'>-</Button>
+            <Button variant='contained' onClick={handleMinus}>-</Button>
             <Typography variant='body1'>{counter}</Typography>
-            <Button variant='contained'>+</Button>
+            <Button variant='contained' onClick={handlePlus}>+</Button>
         </Stack>
     )
 }
